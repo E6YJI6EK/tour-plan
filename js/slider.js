15527c013995447060e9d8b7d25c3c9887037673
E6YJI6EK <nurzhanovaishat@yandex.ru>
2021-11-24 18:28:57 +0400

    create slider

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  }
+});

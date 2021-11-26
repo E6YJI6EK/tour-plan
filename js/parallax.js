d2056ae6f7e992468678575c023865f13d9c9988
E6YJI6EK <nurzhanovaishat@yandex.ru>
2021-11-26 18:18:40 +0400

    parallax newsletter

---
+++ js/parallax.js
@@ -0,0 +1,17 @@
+const scrollHeight = Math.max(
+  document.body.scrollHeight,
+  document.documentElement.scrollHeight,
+  document.body.offsetHeight,
+  document.documentElement.offsetHeight,
+  document.body.clientHeight,
+  document.documentElement.clientHeight
+)
+const screenHeight = screen.height
+const object = document.querySelector('.newsletter')
+let bgPosX = "center"
+let bgPosY = "0%"
+
+window.addEventListener('scroll', () => {
+    bgPosY = (parseInt(scrollY) / scrollHeight) * 100
+    object.style.backgroundPosition = bgPosX + ' ' + bgPosY + "%"
+})

40d22e71821d12b03066f4c3e03f92d121744990
E6YJI6EK <nurzhanovaishat@yandex.ru>
2021-12-09 23:27:53 +0400

    finish 1.2

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,1 +1,37 @@
-$(document).ready(function () { $(".modal__form").validate({ errorClass: "invalid", rules: { mail: { email: !0 } }, messages: { name: { required: "You forgot to say us your name, sir.", minlength: "Your name must be a more then two letters." }, phone: { required: "Also we need your phone." }, mail: { required: "And an email address wouldn't hurt either." } } }), $(".footer__form").validate({ errorClass: "invalid", messages: { name: { required: "You forgot to say us your name, sir.", minlength: "Your name must be a more then two letters." }, phone: { required: "Also we need your phone." } } }), $(".booking-form").validate({ errorClass: "invalid", rules: { mail: { email: !0 } }, messages: { name: { required: "You forgot to say us your name, sir.", minlength: "Your name must be a more then two letters." }, phone: { required: "Also we need your phone." }, mail: { required: "And an email address wouldn't hurt either." } } }), $(".modal__phone").mask("+7 (000) 000-00-00"), $(".footer__phone").mask("+7 (000) 000-00-00") });
+$(document).ready(function () {
+  $(".newsletter__subscribe").validate({
+    errorClass: "newsletter__invalid",
+    messages: {
+      mail: { 
+        required: "Please enter a email address.",
+        email: "Please enter a valid email address. For example: john@abc.com"
+      },
+    },
+  });
+  $(".modal__form").validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "You forgot to say us your name.",
+        minlength: "Your name must be a more then two letters.",
+      },
+      phone: { required: "Also we need your phone." },
+      mail: {
+        required: "And an email address wouldn't hurt either.",
+        email: "Please enter a valid email address. For example: john@abc.com"
+      },
+    },
+  }),
+    $(".footer__form").validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "You forgot to say us your name.",
+          minlength: "Your name must be a more then two letters.",
+        },
+        phone: { required: "Also we need your phone." },
+      },
+    }),
+    $(".modal__phone").mask("+7 (000) 000-00-00"),
+    $(".footer__phone").mask("+7 (000) 000-00-00");
+});
--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,30 @@
-const modalViewOther = document.querySelector(".view_other"), modalBookNow = document.querySelector(".book_now"); modalOpenViewOther = document.querySelector(".booking__btn"), modalOpenBookNow = document.querySelectorAll(".book_btn"), modalOpenViewOther.addEventListener("click", () => { document.body.classList.add("stop-scroll"), modalViewOther.classList.add("modal_visible"), modalViewOther.querySelector(".modal__close").addEventListener("click", o => { modalViewOther.classList.remove("modal_visible"), document.body.classList.remove("stop-scroll") }), window.addEventListener("keydown", o => { "Escape" === o.code && (modalViewOther.classList.remove("modal_visible"), document.body.classList.remove("stop-scroll")) }) }), modalOpenBookNow.forEach(o => { o.addEventListener("click", () => { document.body.classList.add("stop-scroll"), modalBookNow.classList.add("modal_visible"), modalBookNow.querySelector(".modal__close").addEventListener("click", o => { o.preventDefault(), modalBookNow.classList.remove("modal_visible"), document.body.classList.remove("stop-scroll") }), window.addEventListener("keydown", o => { "Escape" === o.code && (modalBookNow.classList.remove("modal_visible"), document.body.classList.remove("stop-scroll")) }) }) });
+const modalViewOther = document.querySelector(".view_other"),
+modalOpenViewOther = document.querySelector(".booking__btn");
+  modalOpenViewOther.addEventListener("click", () => {
+    document.body.classList.add("stop-scroll"),
+      modalViewOther.classList.add("modal_visible");
+      let formName = modalViewOther.querySelector('.modal__name');
+      let formPhone = modalViewOther.querySelector('.modal__phone');
+      let formMail = modalViewOther.querySelector('.modal__mail');
+      let formMessage = modalViewOther.querySelector('.modal__message');
+      modalViewOther
+        .querySelector(".modal__close")
+        .addEventListener("click", (o) => {
+            formName.value = '';
+            formPhone.value = '';
+            formMail.value = '';
+            formMessage.value = '';
+          modalViewOther.classList.remove("modal_visible"),
+            document.body.classList.remove("stop-scroll");
+        });
+      window.addEventListener("keydown", (o) => {
+        if ("Escape" === o.code) {
+            formName.value = '';
+            formPhone.value = '';
+            formMail.value = '';
+            formMessage.value = '';
+            modalViewOther.classList.remove("modal_visible");
+            document.body.classList.remove("stop-scroll");
+        }    
+      });
+  });

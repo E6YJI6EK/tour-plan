db799e1d8fdccef9f5e8d09d4528fe814cb15dfe
E6YJI6EK <nurzhanovaishat@yandex.ru>
2021-12-10 23:29:40 +0400

    finish 1.3

--- js/form-validate.js
+++ js/form-validate.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(".newsletter__subscribe").validate({
     errorClass: "newsletter__invalid",
     messages: {
-      mail: { 
+      subscribe: { 
         required: "Please enter a email address.",
         email: "Please enter a valid email address. For example: john@abc.com"
       },
@@ -10,28 +10,45 @@ $(document).ready(function () {
   });
   $(".modal__form").validate({
     errorClass: "invalid",
+    rules: {
+      mail: {
+        email: true,
+        required: true
+      }
+    },
     messages: {
       name: {
         required: "You forgot to say us your name.",
         minlength: "Your name must be a more then two letters.",
       },
-      phone: { required: "Also we need your phone." },
+      phone: { 
+        required: "Also we need your phone.",
+        minlength: "Please enter a full phone number"
+      },
       mail: {
         required: "And an email address wouldn't hurt either.",
-        email: "Please enter a valid email address. For example: john@abc.com"
+        email: "Please enter valid email address. For example: john@abc.com"
       },
     },
-  }),
-    $(".footer__form").validate({
-      errorClass: "invalid",
-      messages: {
-        name: {
-          required: "You forgot to say us your name.",
-          minlength: "Your name must be a more then two letters.",
-        },
-        phone: { required: "Also we need your phone." },
+  });
+  $(".footer__form").validate({
+    errorClass: "invalid",
+    rules: {
+      name: {
+        required: true
       },
-    }),
-    $(".modal__phone").mask("+7 (000) 000-00-00"),
-    $(".footer__phone").mask("+7 (000) 000-00-00");
+    },
+    messages: {
+      name: {
+        required: "You forgot to say us your name.",
+        minlength: "Your name must be a more then two letters.",
+      },
+      phone: { 
+        required: "Also we need your phone.",
+        minlength: "Please enter a full phone number"
+      },
+    },
+  });
+  $(".modal__phone").mask("+7 (000) 000-00-00"),
+  $(".footer__phone").mask("+7 (000) 000-00-00");
 });
